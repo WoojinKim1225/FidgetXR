15b88400dde56f870478b2dbda650c9fc945f1a7
WoojinKim1225 <kwj69641@gmail.com>
2024-05-02 18:25:14 +0900

    Feat: Added Curved UI

--- Assets/Scripts/XR/Interactable/MeshInteractable.cs
+++ Assets/Scripts/XR/Interactable/MeshInteractable.cs
@@ -20,7 +20,7 @@ public class MeshInteractable : MonoBehaviour
     public float curvature = 0.25f;
     public float resolution = 0.01f;
 
-    public Transform pointer;
+    //public Transform pointer;
 
     
     
@@ -34,8 +34,13 @@ public class MeshInteractable : MonoBehaviour
                 else
                     return null;
             case EMeshType.Curved:
-                Vector3 h = Vector3.ProjectOnPlane(originOS + (1f / curvature) * Vector3.up, directionOS);
-                return (1f / curvature) * Vector3.down + h + directionOS * Mathf.Sqrt(1 - Vector3.SqrMagnitude(h) * curvature * curvature) / curvature;
+                float radius = 1f / curvature;
+                Vector3 horizontalDirOS = Vector3.Normalize(directionOS - Vector3.up * directionOS.y);
+                Vector3 horizontalOriginOS = originOS - Vector3.up * originOS.y;
+                float tan = directionOS.y/ Mathf.Sqrt(directionOS.x * directionOS.x + directionOS.z * directionOS.z);
+                Vector3 h = Vector3.ProjectOnPlane(horizontalOriginOS - radius * Vector3.back, horizontalDirOS);
+                Vector3 horizontalPosOS = radius * Vector3.back + h + horizontalDirOS * Mathf.Sqrt(1 - Vector3.SqrMagnitude(h) * curvature * curvature) * radius;
+                return horizontalPosOS + Vector3.up * (originOS.y + Vector3.Distance(horizontalPosOS, horizontalOriginOS) * tan);
             case EMeshType.Spherical:
                 break;
         }
@@ -51,9 +56,7 @@ public class MeshInteractable : MonoBehaviour
         meshGenerator.curvature = meshType == EMeshType.Flat ? 0f : curvature;
         meshGenerator.resolution = resolution;
 
-        Vector3? hitPosOS = GetHitPointOS(pointer.position, pointer.forward);
-        if (hitPosOS.HasValue)
-            Debug.DrawLine(pointer.position, transform.TransformPoint(hitPosOS.Value), Color.cyan);
+        // Vector3? hitPosOS = GetHitPointOS(pointer.position, pointer.forward);
     }
     
     
