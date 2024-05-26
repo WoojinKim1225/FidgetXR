37225d4d683409a62f1c822c9ff9895b87ee7abf
WoojinKim1225 <kwj69641@gmail.com>
2024-05-27 00:31:52 +0900

    Feat: Adding object interactable

---
+++ Assets/Scripts/XR/Interactable/InteractableHelper.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace EventManager
+{
+    [System.Serializable]
+    public struct StatefulButtonUnityEvent {
+        public UnityEvent OnEnter, OnStay, OnExit, OnClick;
+    }
+
+    [System.Serializable]
+    public struct StatefulAxisUnityEvent {
+        public UnityEvent OnValueChange, 
+    }
+
+    [System.Serializable]
+    public struct FingerInteractable {
+        [HideInInspector]
+        public string name;
+
+        // Curling One Finger
+        public StatefulButtonUnityEvent Curl;
+
+        // Pinching Corresponding Finger with Thumb;
+        public StatefulButtonUnityEvent Pinch;
+
+        // Tip Distance From Object;
+        public StatefulButtonUnityEvent Touch;
+        
+        // Tapping Finger Base with Thumb;
+        public StatefulButtonUnityEvent Tap;
+    }
+
+    [System.Serializable]
+    public struct TangibleInteractable {
+        [HideInInspector]
+        public string name;
+
+        public StatefulAxisUnityEvent pitch, yaw, roll;
+        public StatefulAxisUnityEvent sway, heave, surge;
+    }
+}
--- Assets/Scripts/XR/Interactable/LineInteractable.cs
+++ Assets/Scripts/XR/Interactable/LineInteractable.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using EventManager;
 
 public class LineInteractable : MonoBehaviour
 {
