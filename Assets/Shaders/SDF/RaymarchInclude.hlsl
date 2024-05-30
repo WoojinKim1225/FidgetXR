351964b839d5b5d519e6cf2067d20e75cbd503b5
WoojinKim1225 <kwj69641@gmail.com>
2024-05-30 10:34:43 +0900

    Feat: Ray tracing WIP with mesh renderer interaction
    - Ray tracing functionality WIP
    - Enabled interaction between ray tracing and mesh renderer objects

--- Assets/Shaders/SDF/RaymarchInclude.hlsl
+++ Assets/Shaders/SDF/RaymarchInclude.hlsl
@@ -19,7 +19,7 @@ struct SDFVars {
 };
 
 
-float4 GetDist(float3 p, SDFVars var) {
+float4 GetDist(float3 p) {
     //float d = smoothMin(sphereSDF(p, var.p1.xyz, 0.03), sphereSDF(p, var.p2.xyz, 0.03), 100);
     /*
     float d = min(
@@ -37,7 +37,7 @@ float4 GetDist(float3 p, SDFVars var) {
     */
     //float4 b = float4(0,0,1,sdSphere(p, float3(_SinTime.w,0,0), 0.1));
     //float4 c = float4(0,1,0,sdSphere(p, float3(-_SinTime.w,0,0), 0.2));
-    
+    /*
     float4 b = float4(.5, .5, 1, lerp(
         sdRoundCone(p, var.p_L, var.p_L + var.d_L, var.r1_L, 0.005), 
         sdSphere(p, var.p_L, var.r1_L), 
@@ -53,25 +53,19 @@ float4 GetDist(float3 p, SDFVars var) {
 
     float4 d = sdSmoothUnion(b,c, 0.5);
     return d;
+    */
+    return float4(1, 1, 1, sdSphere(p, float3(0,1.2,1), 0.1));
 }
 
-float3 GetNormal(float3 p, SDFVars var) {
+float3 GetNormal(float3 p) {
     float2 e = float2(1e-2, 0);
-    float3 n = GetDist(p, var).w - float3(GetDist(p + e.xyy, var).w, GetDist(p + e.yxy, var).w, GetDist(p + e.yyx, var).w);
+    float3 n = GetDist(p).w - float3(GetDist(p + e.xyy).w, GetDist(p + e.yxy).w, GetDist(p + e.yyx).w);
 
     return normalize(n);
 }
 
-void Raymarch_half(half3 ro, half3 rd, half3 rdMid, half depth, half3 p_L, half3 p_R, half r1_L, half r1_R, half3 d_L, half3 d_R, half t_L, half t_R, out half3 p, out half3 n, out half3 c, out half a) {
-    SDFVars var;
-    var.p_L = p_L;
-    var.p_R = p_R;
-    var.d_L = d_L;
-    var.d_R = d_R;
-    var.r1_L = r1_L;
-    var.r1_R = r1_R;
-    var.t_L = t_L;
-    var.t_R = t_R;
+void Raymarch_half(float3 ro, float3 rd, float3 camForward, float depth, out half3 p, out half3 n, out half3 c, out half a) {
+
     
     half dO = 0;
     half4 dS;
@@ -80,24 +74,23 @@ void Raymarch_half(half3 ro, half3 rd, half3 rdMid, half depth, half3 p_L, half3
             break;
         }
         half3 p = ro + dO * rd;
-        dS = GetDist(p, var);
+        dS = GetDist(p);
         if (dS.w < SURF_DIST) {
             c = dS.xyz;
             break;
         }
         dO += dS.w;
     }
-    if (dO < MAX_DIST && dO * dot(rd, rdMid) < depth) {
+    if (dO < MAX_DIST && dO * dot(rd, camForward) < depth) {
         p.xyz = ro + rd * dO;
         a = 1;
-        n = GetNormal(p, var);
+        n = GetNormal(p);
     } else {
         p = half3(0,0,0);
         a = 0;
         c = half3(0,0,0);
         n = half3(0,0,0);
     }
-    //return dO;
 }
 
 
